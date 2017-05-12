5137842b5afa512f947662a7025bfd392b5d759e
Learning Laravel <support@learninglaravel.net>
2017-05-13 01:40:56 +0700

    Chapter 4

--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Http, RequestOptions } from '@angular/http';
 import { AuthHttp, AuthConfig } from 'angular2-jwt';
 
-function authHttpServiceFactory(http: Http, options: RequestOptions) {
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
   return new AuthHttp(new AuthConfig(), http, options);
 }
 
